2465ff0c155e820b3fe3f3e6db8717abd941ee5e
Yuval Kanar <cardslay3r@gmail.com>
2018-09-25 22:33:51 +0300

    Added some stuff

---
+++ notes.md
@@ -0,0 +1,11 @@
+## Main effect
+First, letters are randomly generated on the x plane and pushed onto a vector.
+Then, in each iteration, all of the letters are drawn on the canvas and their y axis value is decreased according to the letter gap.
+
+## Rain effect
+Add trailing mechanism in which during x iterations (may be random) following the first letter, letters appear above the previous letters.
+
+## Changing effect
+Each letter may change by chance:
+  If random results in true:
+    Change letter after random number of iterations
--- sketch.js
+++ sketch.js
@@ -1,13 +1,41 @@
+var fontsize = 12;
+
 function setup() {
-    createCanvas(500, 250)
+    createCanvas(
+        window.innerWidth,
+        window.innerHeight
+    );
+    background(0);
+
+    symbol = new Character(width / 2, height / 2);
+    frameRate(1);
 }
 
 function draw() {
-    if (mouseIsPressed) {
-        fill(0);
-    } else {
-        fill(255)
-    }
+    background(0);
+    symbol.setToRandomSymbol();
+    symbol.render();
+}
 
-    ellipse(mouseX, mouseY, 80, 80)
+
+function Character(x, y) {
+    this.x = x;
+    this.y = y;
+    this.value;
+
+    this.setToRandomSymbol = function() {
+        this.value = String.fromCharCode(
+            // Setting using katakana unicode values
+            0x30A0 + round(random(0, 0x30A0 - 0x30F6))
+        );
+    };
+
+    this.render = function() {
+        fill(0, 255, 70);
+        text(this.value, this.x, this.y);
+    };
+}
+
+function CharStream() {
+
 }
